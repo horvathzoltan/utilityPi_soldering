04b0f8f4087505ddc6709113e2faf8316cca25ad
horvathzoltan <horvath.zoltan.mobil@gmail.com>
2020-11-26 19:42:46 +0100

    kettő

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,14 +7,13 @@ MainWindow::MainWindow(QWidget *parent)
       , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(&_timer, SIGNAL(timeout()), this, SLOT(on_timer_timeout()));
-    connect(ui->frame, SIGNAL(paint()), this, SLOT(on_paint()));
+    connect(&_timer, SIGNAL(timeout()), this, SLOT(on_timer_timeout()));    
     //SNPB
-    _heatdata.append({0, 25}); //start
-    _heatdata.append({70, 100}); //
-    _heatdata.append({175, 140});
-    _heatdata.append({239, 220});
-    _heatdata.append({300, 25}); //end
+    _heatdata.append({0, 25, "preheat"}); //start
+    _heatdata.append({70, 100, "soak"}); //
+    _heatdata.append({175, 140, "reflow"});
+    _heatdata.append({240, 220, "cooling"});
+    _heatdata.append({300, 25, "end"}); //end
     //SN
 //    _heatdata.append({0, 25}); //start
 //    _heatdata.append({120, 100}); //
@@ -22,8 +21,16 @@ MainWindow::MainWindow(QWidget *parent)
 //    _heatdata.append({375, 260});
 //    _heatdata.append({600, 25}); //end
 
-    _ra = new RenderArea(ui->frame);
 
+    _ra = new RenderArea(ui->frame);
+    _ra->setPen(QPen(Qt::blue));
+    _ra->setBrush(QBrush(Qt::lightGray));
+    _ra->setPolyLine(HeatData::ToQPointF(_heatdata));
+
+//    static int p = 2;
+//    _ra->setGeometry({p,p,ui->frame->width()-2*p,ui->frame->height()-2*p });
+//    auto b = new QPushButton();
+//    b->setGeometry({0,0,1,1});
 }
 
 MainWindow::~MainWindow()
@@ -36,7 +43,12 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     if(_elapsed) return;
-    ui->label->setText("0");
+
+    ui->label->setText("");
+    _ra->setMarker(0);
+    _ra->setMtext("");
+    _ra->repaint();
+
     _elapsed = new QElapsedTimer();
     _elapsed->start();
     _timer.start(1000);
@@ -45,24 +57,23 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::on_timer_timeout()
 {
     if(!_elapsed) return;
-    QString e = QString::number(_elapsed->elapsed()/1000);
+    qreal mx = _elapsed->elapsed()/1000;
+    QString e = QString::number(mx);
     ui->label->setText(e);
+    _ra->setMarker(mx);
+    QString* txt=nullptr;
+    for(auto& i:_heatdata){if(mx<i.time) break; txt=&(i.desc); }
+    if(txt) _ra->setMtext(*txt);
+    _ra->repaint();
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
+    // auto r = ui->frame->geometry();
     if(!_elapsed) return;
     _timer.stop();
-    delete _elapsed; _elapsed = nullptr;
+    delete _elapsed; _elapsed = nullptr;   
 }
 
-void MainWindow::on_paint(QPaintEvent *e) {
-    ui->label->setText("r");
-//    QPainter paint(this);
-//    paint.setPen(Qt::red);
-//    paint.drawEllipse(10,10,100,100);
 
-//    paint.end();
-//    QFrame::paintEvent(e);
-}
 
--- renderarea.h
+++ renderarea.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QPen>
+#include <QPainter>
 
 class RenderArea : public QWidget
 {
@@ -11,19 +12,29 @@ class RenderArea : public QWidget
 public:
 
     explicit RenderArea(QWidget *parent = nullptr);
+    //~RenderArea() {delete _painter;}
 
     QSize minimumSizeHint() const override;
     QSize sizeHint() const override;
 
-
+    void setPolyLine(const QVarLengthArray<QPointF>& pl){_pl = pl;}
+    void setPen(const QPen& p){_pen = p;}
+    void setBrush(const QBrush& b){_brush = b;}
+    void setMarker(qreal x){_mx = x;}
+    void setMtext(const QString& x){_mtxt = x;}
+    //public slots:
 //public slots:
-    //void setData();
+    //void setData();    
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
-    QPen pen;
-    QBrush brush;
+    //QPainter* _painter=nullptr;
+    QPen _pen;
+    qreal _mx =0;
+    QString _mtxt;
+    QBrush _brush;
+    QVarLengthArray<QPointF> _pl;
 
     //QPixmap pixmap;
 };
